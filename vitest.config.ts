7914bbe14ecf548e47d7e9abb19c618d069bc287
betogroo <luizhumberto@gmail.com>
2025-07-22 09:03:05 -0300

    chore add test on pre-commit

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
+    globals: true,
     environment: 'nuxt',
+    include: ['tests/**/*.test.ts', 'tests/**/*.spec.ts'],
   },
 })
