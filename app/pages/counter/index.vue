d2ae6603c142c272c1e34cf66a1331b03992e166
betogroo <luizhumberto@gmail.com>
2025-07-22 20:39:22 -0300

    feat: add counter layout

--- app/pages/counter/index.vue
+++ app/pages/counter/index.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <counter-display />
+  <v-container class="d-flex justify-center align-center">
+    <v-card class="text-center pa-4" width="400">
+      <div class="d-flex flex-column justify-center align-center">
+        <counter-display />
+        <counter-buttons />
+      </div>
+    </v-card>
+  </v-container>
 </template>
