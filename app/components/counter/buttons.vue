cc2d6da4b08a5032c10bda389d81fac94dc4b9d0
betogroo <luizhumberto@gmail.com>
2025-07-22 20:38:51 -0300

    feat: add cuttons component

---
+++ app/components/counter/buttons.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="d-flex ga-3">
+    <v-btn color="red" variant="outlined">-</v-btn>
+    <v-btn color="blue" variant="outlined">+</v-btn>
+  </div>
+</template>
---
+++ tests/components/counter/buttons.test.ts
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import CounterButtons from '../../../app/components/counter/buttons.vue'
+
+describe('CounterButtons', () => {
+  it('generate expect html', async () => {
+    const wrapper = await mountSuspended(CounterButtons)
+    console.log(wrapper.html()) // Apenas para inspeção
+  })
+  it('render the component', async () => {
+    const wrapper = await mountSuspended(CounterButtons)
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('have two buttons', async () => {
+    const wrapper = await mountSuspended(CounterButtons)
+    const buttons = wrapper.findAll('button')
+    expect(buttons.length).toBe(2)
+  })
+
+  it('button "-" is present and visible', async () => {
+    const wrapper = await mountSuspended(CounterButtons)
+    const minus = wrapper.find('button:first-child')
+    expect(minus.text()).toBe('-')
+  })
+
+  it('botão "+" is present and visible', async () => {
+    const wrapper = await mountSuspended(CounterButtons)
+    const plus = wrapper.find('button:last-child')
+    expect(plus.text()).toBe('+')
+  })
+})
