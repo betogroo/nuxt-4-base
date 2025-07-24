def8e351698c777561db32d3b2214820da87ba75
betogroo <luizhumberto@gmail.com>
2025-07-24 19:28:44 -0300

    feat: add reset button

--- app/components/counter/buttons.vue
+++ app/components/counter/buttons.vue
@@ -3,12 +3,22 @@
   const $emit = defineEmits<{
     increment: []
     decrement: []
+    resetValue: []
   }>()
 </script>
 
 <template>
-  <div class="d-flex ga-3">
-    <v-btn color="red" variant="outlined" @click="$emit('decrement')">-</v-btn>
-    <v-btn color="blue" variant="outlined" @click="$emit('increment')">+</v-btn>
+  <div>
+    <div class="d-flex ga-3">
+      <v-btn color="red" data-testid="decrement" variant="outlined" @click="$emit('decrement')"
+        >-</v-btn
+      >
+      <v-btn color="blue" data-testid="increment" variant="outlined" @click="$emit('increment')"
+        >+</v-btn
+      >
+    </div>
+    <div>
+      <v-btn color="error" data-testid="reset" @click="$emit('resetValue')">Reiniciar</v-btn>
+    </div>
   </div>
 </template>
