10c1a5bc17e98b371640a43cf0a53ffc3aa23dc0
betogroo <luizhumberto@gmail.com>
2025-07-24 14:05:18 -0300

    test(WIP): starting test

--- app/composables/useCounter.test.ts
+++ app/composables/useCounter.test.ts
@@ -2,10 +2,16 @@ import { describe, it, expect } from 'vitest'
 import useCounter from '~/composables/useCounter'
 
 describe('useCounter', () => {
-  it('return 0 as default value', () => {
+  it('return 0 as default value and increment', () => {
     const { displayNumber, incrementValue } = useCounter()
     expect(displayNumber.value).toBe(0)
     incrementValue()
     expect(displayNumber.value).toBe(1)
+  })
+  it('return 0 as default value and decrement', () => {
+    const { displayNumber, decrementValue } = useCounter()
+    expect(displayNumber.value).toBe(0)
+    decrementValue()
+    expect(displayNumber.value).toBe(-1)
   })
 })
