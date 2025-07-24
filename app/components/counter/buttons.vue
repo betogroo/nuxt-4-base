36ac36490b318ca6284d1b0116c4c2bb93984b96
betogroo <luizhumberto@gmail.com>
2025-07-24 13:01:01 -0300

    feat: implement incremmet event

--- app/components/counter/buttons.vue
+++ app/components/counter/buttons.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
   defineOptions({ name: 'CounterButtons' })
+  const $emit = defineEmits<{
+    increment: []
+  }>()
 </script>
 
 <template>
   <div class="d-flex ga-3">
     <v-btn color="red" variant="outlined">-</v-btn>
-    <v-btn color="blue" variant="outlined">+</v-btn>
+    <v-btn color="blue" variant="outlined" @click="$emit('increment')">+</v-btn>
   </div>
 </template>
