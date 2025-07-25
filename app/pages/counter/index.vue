662f3d8a055ee328ec2743cf4b5729a72fa627bb
betogroo <luizhumberto@gmail.com>
2025-07-25 16:57:24 -0300

    feat: implement reset button

--- app/pages/counter/index.vue
+++ app/pages/counter/index.vue
@@ -2,7 +2,7 @@
   definePageMeta({
     name: 'CounterIndex',
   })
-  const { displayNumber, incrementValue, decrementValue } = useCounter()
+  const { displayNumber, incrementValue, decrementValue, resetValue } = useCounter()
 </script>
 
 <template>
@@ -10,7 +10,11 @@
     <v-card class="text-center pa-4" width="400">
       <div class="d-flex flex-column justify-center align-center">
         <counter-display :display-number="displayNumber" />
-        <counter-buttons @decrement="decrementValue" @increment="incrementValue" />
+        <counter-buttons
+          @decrement="decrementValue"
+          @increment="incrementValue"
+          @reset-value="resetValue"
+        />
       </div>
     </v-card>
   </v-container>
