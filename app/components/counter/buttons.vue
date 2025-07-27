13925c8cc2986d4e96af2f7e7c164e89c9a250d1
betogroo <luizhumberto@gmail.com>
2025-07-27 15:35:02 -0300

    refactor: style

--- app/components/counter/buttons.vue
+++ app/components/counter/buttons.vue
@@ -12,33 +12,31 @@
   }>()
 </script>
 <template>
+  <div class="d-flex ga-2">
+    <v-btn
+      color="red"
+      data-testid="decrement"
+      :disabled="disabled.includes('decrement')"
+      variant="outlined"
+      @click="$emit('decrement')"
+      >-</v-btn
+    >
+    <v-btn
+      color="blue"
+      data-testid="increment"
+      :disabled="disabled.includes('increment')"
+      variant="outlined"
+      @click="$emit('increment')"
+      >+</v-btn
+    >
+  </div>
   <div>
-    <div class="d-flex ga-3">
-      <v-btn
-        color="red"
-        data-testid="decrement"
-        :disabled="disabled.includes('decrement')"
-        variant="outlined"
-        @click="$emit('decrement')"
-        >-</v-btn
-      >
-      <v-btn
-        color="blue"
-        data-testid="increment"
-        :disabled="disabled.includes('increment')"
-        variant="outlined"
-        @click="$emit('increment')"
-        >+</v-btn
-      >
-    </div>
-    <div>
-      <v-btn
-        color="error"
-        data-testid="reset"
-        :disabled="disabled.includes('reset')"
-        @click="$emit('reset-value')"
-        >Reiniciar</v-btn
-      >
-    </div>
+    <v-btn
+      color="error"
+      data-testid="reset"
+      :disabled="disabled.includes('reset')"
+      @click="$emit('reset-value')"
+      >Reiniciar</v-btn
+    >
   </div>
 </template>
--- app/pages/counter/index.vue
+++ app/pages/counter/index.vue
@@ -9,7 +9,7 @@
 <template>
   <v-container class="d-flex justify-center align-center">
     <v-card class="text-center pa-4" width="400">
-      <div class="d-flex flex-column justify-center align-center">
+      <div class="d-flex flex-column justify-center align-center ga-4">
         <counter-display :display-number="displayNumber" />
         <h3 data-testid="maxDisplay">Max: {{ maxValue }}</h3>
         <counter-buttons
