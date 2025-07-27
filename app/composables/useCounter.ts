fa4ee764ba1d3e062dc63a57baf98d68a05db4c7
betogroo <luizhumberto@gmail.com>
2025-07-27 13:22:55 -0300

    feat: do not can increment value if nummber is equal masValue

--- app/composables/useCounter.ts
+++ app/composables/useCounter.ts
@@ -2,7 +2,10 @@ const useCounter = () => {
   const displayNumber = ref<number>(0)
   const maxValue = ref<number>(10)
 
-  const incrementValue = () => displayNumber.value++
+  const incrementValue = () => {
+    if (displayNumber.value === maxValue.value) return
+    displayNumber.value++
+  }
   const decrementValue = () => {
     if (displayNumber.value === 0) return
     displayNumber.value--
@@ -10,7 +13,11 @@ const useCounter = () => {
   const resetValue = () => {
     displayNumber.value = 0
   }
-  const disabled = computed(() => (displayNumber.value === 0 ? ['decrement', 'reset'] : []))
+  const disabled = computed(() => {
+    if (displayNumber.value === 0) return ['decrement', 'reset']
+    else if (displayNumber.value === maxValue.value) return ['increment']
+    else return []
+  })
   return { displayNumber, maxValue, incrementValue, decrementValue, resetValue, disabled }
 }
 
