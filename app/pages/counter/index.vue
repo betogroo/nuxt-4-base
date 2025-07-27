e6da38500a88f19c185c04e2965692a95d71b4d7
betogroo <luizhumberto@gmail.com>
2025-07-27 10:38:36 -0300

    refactor: add maxValue ref

--- app/pages/counter/index.vue
+++ app/pages/counter/index.vue
@@ -2,7 +2,7 @@
   definePageMeta({
     name: 'CounterIndex',
   })
-  const { displayNumber, incrementValue, decrementValue, resetValue } = useCounter()
+  const { displayNumber, incrementValue, decrementValue, resetValue, maxValue } = useCounter()
 </script>
 
 <template>
@@ -10,6 +10,7 @@
     <v-card class="text-center pa-4" width="400">
       <div class="d-flex flex-column justify-center align-center">
         <counter-display :display-number="displayNumber" />
+        <h3>Max: {{ maxValue }}</h3>
         <counter-buttons
           @decrement="decrementValue"
           @increment="incrementValue"
