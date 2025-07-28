993ea7950ac4aed813f0bbbbf000ac6b430acc4f
betogroo <luizhumberto@gmail.com>
2025-07-27 21:02:54 -0300

    test(navbar): refactor code structure

--- app/components/nav-bar.test.ts
+++ app/components/nav-bar.test.ts
@@ -1,30 +1,37 @@
 import { mountSuspended } from '@nuxt/test-utils/runtime'
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe, beforeEach } from 'vitest'
 import { NavBar } from '#components'
 
 describe('nav-bar', () => {
+  let wrapper: ReturnType<typeof mountSuspended>
+
+  beforeEach(async () => {
+    wrapper = await mountSuspended(NavBar)
+  })
   it('component exists', async () => {
-    const wrapper = await mountSuspended(NavBar)
     expect(wrapper.exists()).toBe(true)
   })
   it('must contain nav element', async () => {
-    const wrapper = await mountSuspended(NavBar)
     const nav = wrapper.find('nav')
     expect(nav.exists()).toBe(true)
   })
-  it('possui três links com os destinos corretos', async () => {
-    const wrapper = await mountSuspended(NavBar)
-
+  it('possui três links', async () => {
     const links = wrapper.findAll('a')
     expect(links.length).toBe(3)
-
-    expect(links[0].attributes('href')).toBe('/')
-    expect(links[0].text()).toBe('Home')
-
-    expect(links[1].attributes('href')).toBe('/counter')
-    expect(links[1].text()).toBe('Counter')
-
-    expect(links[2].attributes('href')).toBe('/about')
-    expect(links[2].text()).toBe('About')
+  })
+  it('link home exists', async () => {
+    const home = wrapper.get('[data-testid="link-home"]')
+    expect(home.attributes('href')).toBe('/')
+    expect(home.text()).toBe('Home')
+  })
+  it('link counter exists', async () => {
+    const counter = wrapper.get('[data-testid="link-counter"]')
+    expect(counter.attributes('href')).toBe('/counter')
+    expect(counter.text()).toBe('Counter')
+  })
+  it('link about exists', async () => {
+    const about = wrapper.get('[data-testid="link-about"]')
+    expect(about.attributes('href')).toBe('/about')
+    expect(about.text()).toBe('About')
   })
 })
