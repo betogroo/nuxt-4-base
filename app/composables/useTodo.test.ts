8bcbce45ed89eb1bff2ee80de69f8eff36ca5508
betogroo <luizhumberto@gmail.com>
2025-07-28 11:36:22 -0300

    test(wip): add new todo on todo list

--- app/composables/useTodo.test.ts
+++ app/composables/useTodo.test.ts
@@ -5,4 +5,15 @@ describe('Use Todo Composable', () => {
     const { todoList } = useTodo()
     expect(todoList.value).toEqual([])
   })
+  it('add new todo on todo list', async () => {
+    const { todoList, addTodo } = useTodo()
+    expect(todoList.value.length).toBe(0)
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: 'Arrumar a Cama',
+      isDone: false,
+    }
+    addTodo(newTodo)
+    expect(todoList.value.length).toBe(1)
+  })
 })
