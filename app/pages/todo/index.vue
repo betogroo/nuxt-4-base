593cca6d9d7319ddfb43b319ed145b08d48b05d0
betogroo <luizhumberto@gmail.com>
2025-07-28 10:43:41 -0300

    feat: add todo list

--- app/pages/todo/index.vue
+++ app/pages/todo/index.vue
@@ -2,8 +2,15 @@
   definePageMeta({
     name: 'ToDoIndex',
   })
+  const todoList = [
+    {
+      id: crypto.randomUUID(),
+      title: 'Arrumar a Cama',
+      isDone: false,
+    },
+  ]
 </script>
 
 <template>
-  <div />
+  <div v-for="todo in todoList" :key="todo.id" data-testid="todo-list">{{ todo.title }}</div>
 </template>
