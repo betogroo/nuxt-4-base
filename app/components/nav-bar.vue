eb9e17138fc7213072cc94b37912035374544556
betogroo <luizhumberto@gmail.com>
2025-07-27 21:29:22 -0300

    feat: add todo link

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -5,6 +5,7 @@
   <nav>
     <NuxtLink data-testid="link-home" to="/">Home</NuxtLink> |
     <NuxtLink data-testid="link-counter" to="/counter">Counter</NuxtLink> |
+    <NuxtLink data-testid="link-todo" to="/todo">Todo</NuxtLink> |
     <NuxtLink data-testid="link-about" to="/about">About</NuxtLink>
   </nav>
 </template>
