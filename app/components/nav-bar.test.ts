58342603839fc25fa7f115d2a3b0bb0fa11948ce
betogroo <luizhumberto@gmail.com>
2025-07-27 21:28:09 -0300

    test(navbar): add one more link

--- app/components/nav-bar.test.ts
+++ app/components/nav-bar.test.ts
@@ -17,7 +17,7 @@ describe('nav-bar', () => {
   })
   it('possui três links', async () => {
     const links = wrapper.findAll('a')
-    expect(links.length).toBe(3)
+    expect(links.length).toBe(4)
   })
   it('link home exists', async () => {
     const home = wrapper.get('[data-testid="link-home"]')
