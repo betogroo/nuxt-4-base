cf1edd2a01b789ce16d2cb2dfaa59e1a469f4d0f
betogroo <luizhumberto@gmail.com>
2025-07-28 10:42:59 -0300

    test(WIP): renders a todo list

--- app/pages/todo/index.test.ts
+++ app/pages/todo/index.test.ts
@@ -5,7 +5,11 @@ import TodoIndex from '~/pages/todo/index.vue'
 describe('Todo Index Page', () => {
   it('render component correctly', async () => {
     const wrapper = await mountSuspended(TodoIndex)
-
     expect(wrapper.exists()).toBe(true)
+  })
+  it('renders a todo list', async () => {
+    const wrapper = await mountSuspended(TodoIndex)
+    const todo = wrapper.get('[data-testid="todo-list"]')
+    expect(todo.text()).toBe('Arrumar a Cama')
   })
 })
