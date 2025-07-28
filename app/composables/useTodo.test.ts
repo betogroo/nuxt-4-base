aa1c498a1be99ad94cdea8295db171bf0252fdc9
betogroo <luizhumberto@gmail.com>
2025-07-28 11:06:41 -0300

    test(wip): refactor expect

--- app/composables/useTodo.test.ts
+++ app/composables/useTodo.test.ts
@@ -3,6 +3,6 @@ import { describe, it, expect } from 'vitest'
 describe('Use Todo Composable', () => {
   it('todo list exists', async () => {
     const { todoList } = useTodo()
-    expect(todoList).toBe([])
+    expect(todoList.value).toEqual([])
   })
 })
