74868cec532accfedaa6b362934bdc1c18eba043
betogroo <luizhumberto@gmail.com>
2025-07-30 08:14:56 -0300

    test(wip) new todo is not empty

--- app/composables/useTodo.test.ts
+++ app/composables/useTodo.test.ts
@@ -5,11 +5,18 @@ describe('Use Todo Composable', () => {
     const { todoList } = useTodo()
     expect(todoList.value).toEqual([])
   })
+  it('new todo is not empty', async () => {
+    const { todoList, addTodo } = useTodo()
+    expect(todoList.value.length).toBe(0)
+    const newTodo = ''
+    addTodo(newTodo)
+    expect(todoList.value.length).toBe(0)
+  })
   it('add new todo on todo list', async () => {
     const { todoList, addTodo } = useTodo()
     expect(todoList.value.length).toBe(0)
-
-    addTodo('Arrumar a Cama')
+    const newTodo = 'Arrumar a Cama'
+    addTodo(newTodo)
     expect(todoList.value.length).toBe(1)
   })
 })
