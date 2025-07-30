2ba34ff571d21b1f073a56d7d358f6d115971408
betogroo <luizhumberto@gmail.com>
2025-07-30 07:16:16 -0300

    feat: implments add new toto in page

--- app/pages/todo/index.vue
+++ app/pages/todo/index.vue
@@ -2,15 +2,28 @@
   definePageMeta({
     name: 'ToDoIndex',
   })
-  const todoList = [
+
+  const { todoList, addTodo } = useTodo()
+  todoList.value = [
     {
       id: crypto.randomUUID(),
       title: 'Arrumar a Cama',
       isDone: false,
     },
   ]
+
+  const handleSubmit = () => {
+    addTodo(title.value)
+  }
+  const title = ref<string>('')
 </script>
 
 <template>
   <todo-item v-for="todo in todoList" :key="todo.id" data-testid="todo-list" :item="todo" />
+  <div>
+    <v-form @submit.prevent="handleSubmit">
+      <v-text-field v-model="title" data-testid="title" variant="outlined" />
+      <v-btn data-testid="submit" type="submit">Enviar</v-btn>
+    </v-form>
+  </div>
 </template>
