affbd1dc413a636952da233e247ba042bca81a62
betogroo <luizhumberto@gmail.com>
2025-07-31 12:20:04 -0300

    test(wip): emit toggle-icon event

--- app/components/todo/item.test.ts
+++ app/components/todo/item.test.ts
@@ -15,4 +15,20 @@ describe('Todo Item', () => {
     })
     expect(wrapper.text()).toBe('Arrumar a Cama')
   })
+  it('emit "toggle-icon" event on click on is done icon', async () => {
+    const wrapper = await mountSuspended(TodoItem, {
+      props: {
+        item: {
+          id: '',
+          title: 'Arrumar a Cama',
+          isDone: false,
+        },
+      },
+    })
+    const isDoneIcon = wrapper.find('[data-testid="is-done-icon"]')
+    await isDoneIcon.trigger('click')
+
+    expect(wrapper.emitted('toggle-item')).toBeTruthy()
+    expect(wrapper.emitted('toggle-item')?.length).toBe(1)
+  })
 })
