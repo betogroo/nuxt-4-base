bc34ebfbdda7e4166f787bacb0d7154f5a94b5bc
betogroo <luizhumberto@gmail.com>
2025-08-08 09:17:44 -0300

    chore: add vitest setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineVitestConfig({
   test: {
     globals: true,
     environment: 'nuxt',
+    setupFiles: ['tests/setup.ts'],
     // include: ['tests/**/*.test.ts', 'tests/**/*.spec.ts'],
   },
 })
