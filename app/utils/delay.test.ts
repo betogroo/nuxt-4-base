6312563c08cda924979128242edcb4eb12a3ece3
betogroo <luizhumberto@gmail.com>
2025-08-21 08:40:27 -0300

    feat: create delay helper

---
+++ app/utils/delay.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { delay } from './delay'
+
+describe('delay', () => {
+  it('wait time', async () => {
+    const start = Date.now()
+    await delay(100)
+    const end = Date.now()
+    expect(end - start).toBeGreaterThanOrEqual(100)
+  })
+})
---
+++ app/utils/delay.ts
@@ -0,0 +1,4 @@
+export const delay = (time = 800, msg: boolean | string = false): Promise<void> => {
+  if (msg) console.log(`${msg} - ${time}ms delay`)
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
