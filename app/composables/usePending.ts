a16fcad1a8862aef1704d91713baa4faae950d16
betogroo <luizhumberto@gmail.com>
2025-08-21 17:06:23 -0300

    feat: create is pending helper

--- app/composables/usePending.ts
+++ app/composables/usePending.ts
@@ -10,7 +10,13 @@ const usePending = () => {
   const stopPending = () => {
     pendingState.value = { loading: false }
   }
-  return { pendingState, startPending, stopPending }
+  const isPending = (action: string, item?: string | number) => {
+    if (!pendingState.value.loading) return false
+    if (pendingState.value.action !== action) return false
+    if (item !== undefined && pendingState.value.item !== item) return false
+    return true
+  }
+  return { pendingState, startPending, stopPending, isPending }
 }
 
 export default usePending
