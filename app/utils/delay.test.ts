6c80b646458c71fc9d98822abde5e22456d07bd5
betogroo <luizhumberto@gmail.com>
2025-08-21 10:24:59 -0300

    test: add force param

--- app/utils/delay.test.ts
+++ app/utils/delay.test.ts
@@ -4,7 +4,7 @@ import { delay } from './delay'
 describe('delay', () => {
   it('wait time', async () => {
     const start = Date.now()
-    await delay(100)
+    await delay(100, false, true)
     const end = Date.now()
     expect(end - start).toBeGreaterThanOrEqual(100)
   })
