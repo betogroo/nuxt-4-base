947392f8fd17d8025c20c4f6d44a19f0e8e0ec5e
betogroo <luizhumberto@gmail.com>
2025-08-27 08:27:28 -0300

    add user infor and logout/login action

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
   defineOptions({ name: 'NavBar' })
+  const user = useSupabaseUser()
+  console.log(user.value)
   const { logout } = useAuth()
 
   const handleLogout = async () => {
     await logout()
+    return navigateTo('/')
+  }
+
+  const handleLogin = () => {
     return navigateTo('/auth/login')
   }
 </script>
@@ -14,6 +20,11 @@
     <NuxtLink data-testid="link-todo" to="/todo">Todo</NuxtLink> |
     <NuxtLink data-testid="link-zod" to="/zod">Zod</NuxtLink> |
     <NuxtLink data-testid="link-about" to="/about">About</NuxtLink>
-    <v-btn variant="text" @click="handleLogout">Logout</v-btn>
+
+    <template v-if="user">
+      <v-btn variant="text" @click="handleLogout">Logout</v-btn>
+      {{ user.email }}
+    </template>
+    <v-btn v-else variant="text" @click="handleLogin">Login</v-btn>
   </nav>
 </template>
---
+++ app/pages/auth/confirm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  useUserStatus('/')
+
+  await delay(5000, 'Testando o confirm')
+</script>
+
+<template>
+  <div>
+    <h1>Entrando no Sistema</h1>
+    <h2>Aguarde um momento enquanto conferimos a autenticação.</h2>
+  </div>
+</template>
