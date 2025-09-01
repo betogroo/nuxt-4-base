f4f5db2fb9c159c0dc1b286f66e3a251c77036ec
betogroo <luizhumberto@gmail.com>
2025-09-01 10:29:16 -0300

    feat(wip): create handle error with zod verification

--- app/utils/handle-error.ts
+++ app/utils/handle-error.ts
@@ -1,1 +1,20 @@
 import { ZodError } from 'zod'
+import type { AppError } from '~/schemas'
+
+export const handleError = (err: unknown): AppError => {
+  if (err instanceof ZodError) {
+    const issue = err.issues[0]
+    return {
+      message: issue?.message || 'Ocorreu um erro inesperado',
+      field: issue?.path.join('.') || undefined,
+      code: 'VALIDATION_ERROR',
+      cause: err,
+    }
+  }
+
+  return {
+    message: 'Ocorreu um erro inesperado.',
+    code: 'UNKNOWN_ERROR',
+    cause: err,
+  }
+}
