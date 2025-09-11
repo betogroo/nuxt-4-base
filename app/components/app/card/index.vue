5dddd18eaf393f60ebcb5df53760f3114e5c4fd2
betogroo <luizhumberto@gmail.com>
2025-09-11 15:24:20 -0300

    chore: create app card

---
+++ app/components/app/card/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  interface Props {
+    variant?: 'outlined' | 'tonal'
+  }
+  const { variant = 'outlined' } = defineProps<Props>()
+</script>
+
+<template>
+  <v-card :variant="variant"><slot /></v-card>
+</template>
